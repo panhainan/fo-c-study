a124384c93e8b3f9292db4ca31a2ced33362b481
panhainan <phnlove@163.com>
2017-03-22 00:11:56 +0800

    修改book项目结构

--- book/book/main.c
+++ book/book/main.c
@@ -98,7 +98,7 @@ test_sprint();
 test_scanf();
 main() {
 	//chapter01();
-	chapter02();
+	//chapter02();
 
 
 	//pointer();
@@ -109,7 +109,7 @@ main() {
 
 	//test_print();
 	//test_sprint();
-	//test_scanf();
+	test_scanf();
 }
 /*
 命令行参数程序1 
