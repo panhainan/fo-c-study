61b8b65e7ab4a4a0070fab854cea379b1e527ec3
panhainan <phnlove@163.com>
2017-08-31 09:50:11 +0800

    ignore test

--- data-structure/data-structure/test.c
+++ data-structure/data-structure/test.c
@@ -1,12 +1,167 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MAXSTRLEN 255
+typedef struct {
+	int data;
+	struct NN *left, *right;
+} NN ;
+typedef struct {
+	NN **base;
+	NN **top;
+	int size;
+} SS;
+
+void ttest() {
+
+	SS *S = (SS*)malloc(sizeof(SS));
+	S->base = (NN**)malloc(sizeof(NN*));
+	S->top = S->base;
+	S->size = 2;
+	NN *p = (NN*)malloc(sizeof(NN));
+
+	NN *q = (NN*)malloc(sizeof(NN));
+	q->data = 60;
+	q->right = q->left = NULL;
+
+	NN *w = (NN*)malloc(sizeof(NN));
+	w->data = 30;
+	w->right = w->left = NULL;
+
+	NN *o = (NN*)malloc(sizeof(NN));
+	o->data = 10;
+	o->right = o->left = NULL;
+
+	NN *v = (NN*)malloc(sizeof(NN));
+	v->data = 40;
+	v->right = w;
+	v->left = o;
+
+	p->data = 100;
+	p->left = q;
+	p->right = v;
+
+	*(S->top) = p;
+	*(S->top)++;
+	*(S->top) = q;
+	*(S->top)++;
+	*(S->top) = v;
+	*(S->top)++;
+	*(S->top) = o;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = p;
+	*(S->top)++;
+	*(S->top) = q;
+	*(S->top)++;
+	*(S->top) = v;
+	*(S->top)++;
+	*(S->top) = o;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = p;
+	*(S->top)++;
+	*(S->top) = q;
+	*(S->top)++;
+	*(S->top) = v;
+	*(S->top)++;
+	*(S->top) = o;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+	*(S->top) = w;
+	*(S->top)++;
+
+
+
+
+
+
+	/*
+	int *x = (int *)malloc(sizeof(int));
+	*x = 9001;
+	int **y = (int **)malloc(sizeof(int*));
+	*y = x;
+	*x = 5620;
+	*y++;
+	int *z = (int *)malloc(sizeof(int));
+	*z = 123;
+	*y = z;
+	*y++;
+	int *m = (int *)malloc(sizeof(int));
+	*m = 2103;
+	*y = m;
+	*y++;
+	int *l = (int *)malloc(sizeof(int));
+	*l = 676;
+	*y = l;
+	*y++;
+	int *s = (int *)malloc(sizeof(int));
+	*s = 454565;
+	*y = s;
+	*y++;
+	int *n= (int *)malloc(sizeof(int));
+	n = *(y-1);
+	*/
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 typedef unsigned char SString[MAXSTRLEN + 1];//0号单元存放串的长度
 
 typedef struct {
 	char *ch;
 	int length;
 } HString;
+
 void get_nextS(SString T, int next[]) {
 	//KMP模式匹配算法的next数组值
 	int i = 1, j = 0, t_len = T[0]-48;
